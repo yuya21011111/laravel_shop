0bf7a3c4b3cb92eb751280ec7fe96e0aa0fb8d65
YuyaYamazi <kaimomo12345@gmail.com>
2023-01-29 03:10:42 +0900

    resources

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>{{ config('app.name', 'Greens') }}</title>
 
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
@@ -14,6 +14,8 @@
         {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
          <!-- Styles -->
          <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+         <!-- Favicon -->
+         <link rel="shortcut icon" type="image/x-icon"  href="{{ asset('images/Greens.ico') }}">
 
          <!-- Scripts -->
          <script src="{{ asset('js/app.js') }}" defer></script>
--- routes/admin.php
+++ routes/admin.php
@@ -9,6 +9,7 @@ use App\Http\Controllers\Admin\Auth\NewPasswordController;
 use App\Http\Controllers\Admin\Auth\PasswordResetLinkController;
 use App\Http\Controllers\Admin\Auth\RegisteredUserController;
 use App\Http\Controllers\Admin\Auth\VerifyEmailController;
+use App\Http\Controllers\Admin\OwnersController;
 
 /*
 |--------------------------------------------------------------------------
@@ -24,6 +25,9 @@ use App\Http\Controllers\Admin\Auth\VerifyEmailController;
 Route::get('/', function () {
     return view('admin.welcome');
 });
+
+Route::resource('owners', OwnersController::class)
+  ->middleware(['auth:admin']);
 
 Route::get('/dashboard', function () {
     return view('admin.dashboard');
