32b505634e0fd0e064b8e36f99aba1e73dea0532
YuyaYamazi <kaimomo12345@gmail.com>
2023-01-31 05:10:13 +0900

    commit

--- app/Http/Controllers/Admin/OwnersController.php
+++ app/Http/Controllers/Admin/OwnersController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use App\Models\Owner;
 use Illuminate\Support\Facades\DB;
 use Carbon\Carbon;
+use Illuminate\Support\Facades\Hash;
 
 class OwnersController extends Controller
 {
@@ -40,7 +41,19 @@ class OwnersController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:owners'],
+            'password' => ['required', 'string', 'confirmed', 'min:8'],
+        ]);
+
+        Owner::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        return redirect()->route('admin.owners.index');
     }
 
     /**
