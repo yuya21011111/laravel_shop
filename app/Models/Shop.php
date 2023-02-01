f7195efec4f16bc785408ed64c91713d3ad9b980
YuyaYamazi <kaimomo12345@gmail.com>
2023-02-02 01:30:50 +0900

    Shop

--- app/Models/Shop.php
+++ app/Models/Shop.php
@@ -4,8 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Owner;
 
 class Shop extends Model
 {
     use HasFactory;
+
+    public function owner() {
+        return $this->belongsTo(Owner::class);
+    }
 }
