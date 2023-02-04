e5bb61f8a1479892e1781551fb6b16f4b8cce774
YuyaYamazi <kaimomo12345@gmail.com>
2023-02-05 04:09:43 +0900

    Image

--- routes/owner.php
+++ routes/owner.php
@@ -10,7 +10,7 @@ use App\Http\Controllers\Owner\Auth\PasswordResetLinkController;
 use App\Http\Controllers\Owner\Auth\RegisteredUserController;
 use App\Http\Controllers\Owner\Auth\VerifyEmailController;
 use App\Http\Controllers\Owner\ShopController;
-
+use App\Http\Controllers\Owner\ImageController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -32,6 +32,9 @@ Route::prefix('shops')->
     Route::get('edit/{shop}',[ShopController::class, 'edit'])->name('shops.edit');
     Route::post('update/{shop}',[ShopController::class, 'update'])->name('shops.update');
 });
+
+Route::resource('images', ImageController::class)
+  ->middleware(['auth:owners'])->except(['show']);
 
 Route::get('/dashboard', function () {
     return view('owner.dashboard');
