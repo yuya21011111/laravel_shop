ba0026ae926d18cc78b0e9ef5cc49cbd2ab99c5a
YuyaYamazi <kaimomo12345@gmail.com>
2023-02-06 05:43:00 +0900

    store

--- resources/views/owner/images/index.blade.php
+++ resources/views/owner/images/index.blade.php
@@ -13,6 +13,7 @@
                     <div class="flex justify-end mb-2">
                         <button onclick="location.href='{{ route('owner.images.create') }}'"  class="text-white bg-pink-500 border-0 py-2 px-8 focus:outline-none hover:bg-pink-600 rounded text-lg">新規登録</button>
                         </div>
+                <div class="flex flex-wrap">
                    @foreach ($images as $image)
                    <div class="w-1/4 p-4">
                      <a href="{{ route('owner.images.edit', ['image' => $image->id]) }}">
@@ -20,11 +21,12 @@
                       <div class="text-xl">
                         {{ $image->title }}
                       </div>
-                      <x-thumbnail :filename="$shop->filename" type="products" />
+                      <x-thumbnail :filename="$image->filename" type="products" />
                      </div>
                      </a>
                    </div>
                    @endforeach
+                </div>
                    {{ $images->links() }}
                 </div>
             </div>
