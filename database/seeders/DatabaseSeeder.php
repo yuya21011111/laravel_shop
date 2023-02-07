7e32f88dfa6cef8eeb071b39b6729d27600038b9
YuyaYamazi <kaimomo12345@gmail.com>
2023-02-08 00:03:58 +0900

    stock

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -23,7 +23,8 @@ class DatabaseSeeder extends Seeder
             ShopSeeder::class,
             ImageSeeder::class,
             CategorySeeder::class,
-            ProductSeeder::class
+            ProductSeeder::class,
+            StockSeeder::class
         ]);
     }
 }
