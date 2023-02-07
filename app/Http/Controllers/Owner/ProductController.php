de6ed8210c56e2ce99d3e269b11be5c3f2afcd42
YuyaYamazi <kaimomo12345@gmail.com>
2023-02-07 16:14:35 +0900

    n+1,measures

--- app/Http/Controllers/Owner/ProductController.php
+++ app/Http/Controllers/Owner/ProductController.php
@@ -4,17 +4,41 @@ namespace App\Http\Controllers\Owner;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Image;
+use App\Models\Product;
+use App\Models\SecondaryCategory;
+use App\Models\Owner;
 
 class ProductController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+    public function __construct()
+    {
+        $this->middleware('auth:owners');
+
+        $this->middleware(function ($request, $next) {
+            $id = $request->route()->parameter('image');
+            if(!is_null($id)) {
+                $productsOwnerId = Product::findOrFail($id)->shop->owner->id;
+                $productId = (int)$productsOwnerId; // 文字を数値に変換
+                
+                if($productId !== Auth::id()) {
+                    abort(404);
+                }
+            }
+           return $next($request);
+        });
+    }
+        
     public function index()
     {
-        //
+        // $products = Owner::findOrFail(Auth::id())->shop->product;
+
+        $ownerInfo = Owner::with('shop.product.imageFirst')
+        ->where('id',Auth::id())
+        ->get();
+        
+        return view('owner.products.index',compact('ownerInfo'));
     }
 
     /**
