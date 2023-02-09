b0363a73d8d39d7fba8a4ea69287d47a9df35c0f
YuyaYamazi <kaimomo12345@gmail.com>
2023-02-10 05:46:54 +0900

    product,create

--- resources/views/components/select-image.blade.php
+++ resources/views/components/select-image.blade.php
@@ -16,7 +16,7 @@
   }
 @endphp
 <div class="modal micromodal-slide" id="{{ $modal }}" aria-hidden="true">
-    <div class="modal__overlay" tabindex="-1" data-micromodal-close>
+    <div class="modal__overlay z-50" tabindex="-1" data-micromodal-close>
       <div class="modal__container" role="dialog" aria-modal="true" aria-labelledby="{{ $modal }}-title">
         <header class="modal__header">
           <h2 class="text-xl text-gray-700" id="{{ $modal }}-title">
