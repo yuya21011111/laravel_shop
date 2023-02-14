7259db780154f6b42c0a94e032219d089d2c79c0
YuyaYamazi <kaimomo12345@gmail.com>
2023-02-14 17:10:42 +0900

    cart

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use App\Models\Product;
 
 class User extends Authenticatable
 {
@@ -44,4 +45,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function products() {
+        return $this->belongsToMany(Product::class, 'carts')
+        ->withPivot(['id','quantity']);
+    }
 }
