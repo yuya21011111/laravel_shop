9f4fedfa94ad181952856449bb8fbd8c0dda7091
YuyaYamazi <kaimomo12345@gmail.com>
2023-02-23 05:11:59 +0900

    細かな調整5.9.9.2

--- resources/views/components/select-image.blade.php
+++ resources/views/components/select-image.blade.php
@@ -34,9 +34,9 @@
               <div class="border rounded-md p-2 md:p-4">
              <img class="image" data-id="{{ $name }}_{{ $image->id }}"
                         data-file="{{ $image->filename }}"
-                        data-path="{{ asset('storage/products/') }}"
+                        data-path="{{ asset('products/') }}"
                         data-modal="{{ $modal }}"
-                        src="{{ asset('storage/products/' . $image->filename)}}">
+                        src="{{ asset('products/' . $image->filename)}}">
                  <div class="text-gray-700">
                      {{ $image->title }}
                    </div>
@@ -57,7 +57,7 @@
   <div class="flex justify-around items-center mb-4">
     <a class="px-2 py-1 bg-green-400 text-white font-semibold rounded hover:bg-green-500" data-micromodal-trigger="{{ $modal }}" href='javascript:;'>ファイルを選択</a>
     <div class="w-1/4">
-    <img id="{{ $name }}_thumbnail" @if($cImage) src="{{ asset('storage/products/' . $cImage)}}" @else src="" @endif>
+    <img id="{{ $name }}_thumbnail" @if($cImage) src="{{ asset('products/' . $cImage)}}" @else src="" @endif>
     </div>
   </div>
   <input id="{{ $name }}_hidden" type="hidden" name="{{ $name }}" value="{{ $cId }}">
