5f5bda398cbfb8ae8293a5c88e0eab13ec313004
YuyaYamazi <kaimomo12345@gmail.com>
2023-02-23 04:59:37 +0900

    細かな調整5.9.9

--- resources/views/components/auth-card.blade.php
+++ resources/views/components/auth-card.blade.php
@@ -1,4 +1,4 @@
-<div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0" style="background-image: url('/images/login.png')">
+<div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-[image-url('/images/login.png')]">
     <div>
         {{ $logo }}
     </div>
