d257d4719f99f6006baaed00ae63290cc2092605
YuyaYamazi <kaimomo12345@gmail.com>
2023-02-22 23:53:03 +0900

    細かな調整4

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -31,13 +31,22 @@
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
-            @if(auth('admin')->user())
+            {{-- @if(auth('admin')->user())
                 @include('layouts.admin-navigation')
             @elseif(auth('owners')->user())
                 @include('layouts.owner-navigation')
             @elseif(auth('users')->user())
                 @include('layouts.user-navigation')
-            @endif
+            @endif --}}
+
+          @if(request()->is('admin*'))
+              @include('layouts.admin-navigation')
+          @elseif(request()->is('owner*'))
+              @include('layouts.owner-navigation')
+          @else
+              @include('layouts.user-navigation')
+          @endif
+
 
             <!-- Page Heading -->
             <header class="bg-white shadow">
